f3eff61af81a1443329d2a2fe3017b58fc64e9e0
fcalderon <fcalderon@switchfly.com>
2016-07-24 15:27:57 -0600

    initial commit

---
+++ src/main/java/FahrenheitCelciusConverter.java
@@ -0,0 +1,12 @@
+/**
+ * Created by fcalderon on 7/24/16.
+ */
+public class FahrenheitCelciusConverter {
+    public static int toCelcius(int fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static int toFahrenheit(int celcius) {
+        return celcius * 9 / 5 + 32;
+    }
+}
